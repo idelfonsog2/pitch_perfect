673069ee34c5b72cf90a6541601de7a50562c784
idelfonsog2 <idelfonsog2@gmail.com>
2016-08-19 11:30:27 -0400

    PitchPerfct first commit

---
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -0,0 +1,122 @@
+//
+//  RecordSoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Idelfonso Gutierrez Jr. on 8/18/16.
+//  Copyright © 2016 idelfonso. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import LocalAuthentication
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    
+    var audioRecorder: AVAudioRecorder!
+    
+    override func viewWillAppear(animated: Bool) {
+        print("viewWillaPPER on screen")
+        
+    }
+    
+    func authenticateUser()
+    {
+        let context = LAContext()
+        var error: NSError?
+        let myLocalizedReasonString = "Authentication is required"
+        
+        //check if the divice is compatible with touch ID
+        if context.canEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, error: &error)
+        {
+            context.evaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString, reply: { (success : Bool, evaluationError : NSError?) -> Void in if success { NSOperationQueue.mainQueue().addOperationWithBlock({()-> Void in self.loadData()}) } })
+        }
+        
+    }
+    
+    func loadData() {
+        print("Load Data")
+    }
+    
+    func showPasswordAlert() {}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopRecordingButton.enabled = false
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        authenticateUser()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func recordAudio(sender: UIButton) {
+        print("recorDButton was preessed")
+        recordingLabel.text = "Recording in progress"
+        stopRecordingButton.enabled = true
+        recordButton.enabled = false
+        
+        //
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
+    }
+    
+    @IBAction func stopRecording(sender: AnyObject)
+    {
+        print("stopButton was preessed")
+        recordButton.enabled = true
+        stopRecordingButton.enabled = false
+        recordingLabel.text = "Tap to Record"
+        
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool)
+    {
+            print("AVAudioRecorded Finish Recording"
+        )
+            if flag {
+                self.performSegueWithIdentifier("stopRecording", sender: audioRecorder.url)
+            }
+            else
+            {
+                print("Saving of recording Failed")
+            }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+        if (segue.identifier == "stopRecording")
+        {
+            let playSoundsVC = segue.destinationViewController as! PlaySoundsViewController
+            let recordedAudioURL = sender as! NSURL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
+
+}
+
